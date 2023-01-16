b2216ec1f8e28a050b39e55285f2650a54fbb941
burhanahmeed <burhanahmeed@gmail.com>
2023-01-16 21:57:31 +0700

    add rouutes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,14 @@ import express from 'express';
 import user from '../controllers/UsersController';
 const router = express.Router();
 
+router.get('/me', user.me);
 router.get('/:id', user.get);
 router.get('/', user.list);
+
+router.post('/', user.create);
+router.post('/auth', user.auth);
+router.post('/password', user.updatePassword);
+
+router.put('/:id', user.update);
 
 export default router;
